aa30576c101155d8167fdc31b2592174debb44c4
Fabyo Kock <fabyokock@gmail.com>
2025-09-03 20:52:08 -0300

    Atualização

--- JavaScript-ADA/aula01.js
+++ JavaScript-ADA/aula01.js
@@ -5,17 +5,17 @@ let mediaPedro = (prova1 + prova2 + prova3) / 3;
 
 console.log(`A média do aluno Pedro é ${mediaPedro}.`);
 
-// let a = "pedro" * "paulo";
-// console.log(soma);
+// // let a = "pedro" * "paulo";
+// // console.log(soma);
 
-let contador = 500;
-// console.log(contador);
-// ++contador;
-console.log(contador++);
+// let contador = 500;
+// // console.log(contador);
+// // ++contador;
 // console.log(contador++);
-// console.log(contador);
+// // console.log(contador++);
+// // console.log(contador);
 
-let x = 5;
-console.log(++x); // 6 (incrementa antes de imprimir)
-console.log(x++); // 6 (imprime, depois incrementa)
-console.log(x); // 7
+// let x = 5;
+// console.log(++x); // 6 (incrementa antes de imprimir)
+// console.log(x++); // 6 (imprime, depois incrementa)
+// console.log(x); // 7
