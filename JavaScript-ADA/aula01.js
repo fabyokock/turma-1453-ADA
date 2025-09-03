6ace856237cdc2ec70207ef844d8f7a7c5135c09
Fabyo Kock <fabyokock@gmail.com>
2025-09-02 22:05:07 -0300

    Criação Aula 01

---
+++ JavaScript-ADA/aula01.js
@@ -0,0 +1,21 @@
+let prova1 = 6.0;
+let prova2 = 8.3;
+let prova3 = 5.5;
+let mediaPedro = (prova1 + prova2 + prova3) / 3;
+
+console.log(`A média do aluno Pedro é ${mediaPedro}.`);
+
+// let a = "pedro" * "paulo";
+// console.log(soma);
+
+let contador = 500;
+// console.log(contador);
+// ++contador;
+console.log(contador++);
+// console.log(contador++);
+// console.log(contador);
+
+let x = 5;
+console.log(++x); // 6 (incrementa antes de imprimir)
+console.log(x++); // 6 (imprime, depois incrementa)
+console.log(x); // 7
