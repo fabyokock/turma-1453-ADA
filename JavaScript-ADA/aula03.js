5baa97980f56f4f6ecdd4c682ceeaae56a8a2cd6
Fabyo Kock <fabyokock@gmail.com>
2025-09-13 07:35:14 -0300

    Atualização

--- JavaScript-ADA/aula03.js
+++ JavaScript-ADA/aula03.js
@@ -20,3 +20,61 @@ function resultadoFinal(nome, n1, n2, n3) {
 }
 
 resultadoFinal("Pedro", 6, 8, 5.5);
+
+console.log("-----------------------------------------------");
+console.log("             DICIONÁRIO - OBJETOS              ");
+console.log("-----------------------------------------------");
+
+const statusColors = {
+  info: "#3498db", // azul
+  success: "#2ecc71", // verde
+  warning: "#f1c40f", // amarelo
+  error: "#e74c3c", // vermelho
+  neutral: "#95a5a6", // cinza
+};
+
+// const responseStatus = "info";
+
+console.log(statusColors["info"]);
+
+console.log(statusColors.warning);
+
+const ptToEn = {
+  olá: "Hello",
+  "bem-vindo": "Welcome",
+};
+
+console.log(ptToEn["olá"]);
+
+ptToEn["teste"] = "valor";
+console.log(ptToEn);
+console.log(ptToEn["teste"]);
+
+console.log("-----------------------------------------------");
+console.log("               DICIONÁRIO - MAP                ");
+console.log("-----------------------------------------------");
+
+// const notasAlunos = new Map();
+
+// notasAlunos.set("João", 8.5);
+// notasAlunos.set("Maria", 9.2);
+// notasAlunos.set("Pedro", 7.8);
+
+// console.log("Nota da Maria:", notasAlunos.get("Maria")); // 9.2
+
+// console.log("Jorge está na lista?", notasAlunos.has("Jorge")); // true
+
+// notasAlunos.delete("Pedro");
+
+// console.log(notasAlunos);
+
+// console.log("Quantidade de alunos:", notasAlunos.size);
+
+// // Limpando o Map
+
+const alunos = new Map([
+  ["Joao", 8],
+  ["Maria", 9],
+  ["Antonio", 10],
+]);
+console.log(alunos);
